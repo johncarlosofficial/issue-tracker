33f49b47b940464dc7043e61e7864735fcf58364
John Carlos <johncarlosdeveloper@gmail.com>
2024-08-18 16:31:53 -0300

    🐛 Fix hydration bug on the issues/new page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
+import { useState, useEffect } from "react";
 import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
@@ -14,25 +14,39 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [SimpleMDE, setSimpleMDE] = useState<any>(null);
+
+  useEffect(() => {
+    // Dynamically import SimpleMDE only on the client-side
+    import("react-simplemde-editor").then((mod) => {
+      setSimpleMDE(mod.default);
+    });
+  }, []);
 
   return (
     <form
       className="max-w-xl space-y-3"
       onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
+        try {
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+        } catch (error) {
+          console.log(error);
+        }
       })}
     >
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+      {SimpleMDE && (
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+      )}
       <Button>Submit New Issue</Button>
     </form>
   );
